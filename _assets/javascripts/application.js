c7fbddec59ca26e0791b16403b50cb6adb30ddba
Falko Joseph <falkojoseph@gmail.com>
2017-01-03 17:22:37 +0100

    Layout

--- _assets/javascripts/application.js
+++ _assets/javascripts/application.js
@@ -1,1 +1,25 @@
 //= require_self
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+
+  var startId = -1;
+  setInterval(function() {
+    ++startId;
+    if (startId <= 10) {
+      $listItem = $('[data-id='+startId+']');
+      $listItem.addClass('is-added');
+    }
+  }, 500);
+});
